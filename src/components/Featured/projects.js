5dac1d344383f3b84ef46aebde0e904d94d6fb7e
John Hodges <jdhmtl@users.noreply.github.com>
2022-02-23 13:19:12 -0500

    Add links to featured projects

--- src/components/Featured/projects.js
+++ src/components/Featured/projects.js
@@ -4,8 +4,8 @@ import gitParseImage from "../../images/featured/git-parse.png"
 
 const projects = [
   {
-    description: `<code>git-parse</code> allows JavaScript developers
-      to treat a git repository as they would treat a regular object. 
+    description: `<a href="https://github.com/wayfair/git-parse"><code>git-parse</code></a> allows JavaScript developers
+      to treat a git repository as they would treat a regular object.
       This data structure simplifies git operations and manipulation
       of state without having to write boilerplate <code>git</code> cli
       functions within their project. We've noted dependencies for this project
@@ -16,7 +16,7 @@ const projects = [
     alt: "Git Parse sample image",
   },
   {
-    description: `<code>tremor</code> is Wayfair's solution to ingesting
+    description: `<a href="https://github.com/tremor-rs"><code>tremor</code></a> is Wayfair's solution to ingesting
       data signals from production traffic at the billion-dollar enterprise scale.
       By using Rust instead of our previous Java-based solution, Wayfair saw significant
       gains in computational leverage and throughput for our infrastructure.`,
@@ -24,7 +24,7 @@ const projects = [
     alt: "tremor sample image",
   },
   {
-    description: `<code>extra-model</code> is an implementation of the 
+    description: `<a href="https://github.com/wayfair-incubator/extra-model"><code>extra-model</code></a> is an implementation of the
       <a href="https://aclanthology.org/D18-1384/">ExtRA
       algorithm</a>. Unsupervised, the algorithm executes
       an NLP task called Aspect-Based Sentiment Analysis, used routinely in production on more
